00df41dba2b5b56951eb02eb23fdce7f64276d2e
Anthony Candaele <anthony.candaele@tothepoint.company>
2022-05-08 16:05:46 +0200

    remove conditional in slug.js

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -29,8 +29,6 @@ const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
 export default function OneRecipe({ data, preview }) {
   const router = useRouter();
 
-  if (!data) return <div>Loading...</div>;
-
   const { data: recipe } = usePreviewSubscription(recipeQuery, {
     params: { slug: data.recipe?.slug.current },
     initialData: data,
